1888f5432d87870b7c90078c0dbe5f4918f14358
Michael Wang <michael.wang.physics@gmail.com>
2025-11-02 04:22:42 +0100

    minor optimization

--- src/interactions.jl
+++ src/interactions.jl
@@ -183,13 +183,16 @@ end
 ###########################################################################################################################################
 
 @inline function correct_for_periodicity(Δx::Float64, Δy::Float64, Δz::Float64, box::SVector{3, Float64})
-    if abs(Δx) > 0.5 * box[1]
+    L = 0.5 * box[1]
+    if Δx > L || Δx < -L
         Δx -= sign(Δx) * box[1]
     end
-    if abs(Δy) > 0.5 * box[2]
+    L = 0.5 * box[2]
+    if Δy > L || Δy < -L
         Δy -= sign(Δy) * box[2]
     end
-    if abs(Δz) > 0.5 * box[3]
+    L = 0.5 * box[3]
+    if Δz > L || Δz < -L
         Δz -= sign(Δz) * box[3]
     end
 
